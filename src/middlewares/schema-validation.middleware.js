10b7fb25adf473f63968cf003dfc2a492daf6520
Eduarda Barboza <duuda2011@gmail.com>
2022-12-21 22:51:27 -0300

    refactor: modifica o status de retorno de erro

--- src/middlewares/schema-validation.middleware.js
+++ src/middlewares/schema-validation.middleware.js
@@ -1,15 +1,14 @@
 export function SchemaValidation(schema) {
-    return (req, res, next) => {
-      const params = req.body;
-      const {error} = schema.validate(params, {abortEarly: false});
-      
-      if(error){
-          const errors = error.details.map((detail) => detail.message);
-          return res.status(400).send(errors);
-      }
-  
-      next();
+  return (req, res, next) => {
+    const params = req.body;
+    const {error} = schema.validate(params, {abortEarly: false});
+    
+    if(error){
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
     }
+
+    next();
   }
-  Footer
+}
   
