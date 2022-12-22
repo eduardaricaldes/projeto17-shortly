83d564ea2cea61945989df21d81abd48c789e314
Eduarda Barboza <duuda2011@gmail.com>
2022-12-21 22:52:59 -0300

    feat: criado index do projeto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import config from './config/configuration.js';
+import accountRoute from './routes/account.route.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(accountRoute)
+
+const port = config.APP_PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${config.APP_NAME} running on port ${port}` );
+});
