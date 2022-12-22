4aaeb0b1b09bd2755768e7e27b6d6d36b1440b22
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-12-22 16:21:15 -0300

    feat: adicionado url de ranking com controller

--- src/routes/urls.route.js
+++ src/routes/urls.route.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 import { SchemaValidation } from "../middlewares/schema-validation.middleware.js";
-import { CreateShortUrlController, GetShortUrlByIdController, RedirectController, DeleteShortByIdController, GetUsersMeController} from "../controllers/urls.controller.js";
+import {
+  CreateShortUrlController,
+  GetShortUrlByIdController,
+  RedirectController,
+  DeleteShortByIdController,
+  GetUsersMeController,
+  GetRankingController,
+} from "../controllers/urls.controller.js";
 import { CreateShortUrlSchema } from "../schemas/urls.schema.js";
 const router = Router();
 
@@ -10,5 +17,6 @@ router.get("/urls/:id", GetShortUrlByIdController);
 router.get("/urls/open/:shortUrl", RedirectController);
 router.delete("/urls/:id",AuthMiddleware,DeleteShortByIdController );
 router.get("/users/me",AuthMiddleware, GetUsersMeController);
+router.get("/ranking", GetRankingController)
 
 export default router;              
