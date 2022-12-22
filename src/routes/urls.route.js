7022cef27dc6b005c234e94d754fb00c607249c8
Eduarda Barboza <duuda2011@gmail.com>
2022-12-22 00:39:41 -0300

    feat: adicionado incremeto route

--- src/routes/urls.route.js
+++ src/routes/urls.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 import { SchemaValidation } from "../middlewares/schema-validation.middleware.js";
-import { CreateShortUrlController, GetShortUrlByIdController } from "../controllers/urls.controller.js";
+import { CreateShortUrlController, GetShortUrlByIdController, RedirectController } from "../controllers/urls.controller.js";
 import { CreateShortUrlSchema } from "../schemas/urls.schema.js";
 const router = Router();
 
 router.post("/urls/shorten", [AuthMiddleware, SchemaValidation(CreateShortUrlSchema)], CreateShortUrlController);
 router.get("/urls/:id", GetShortUrlByIdController);
+router.get("/urls/open/:shortUrl", RedirectController);
+
 export default router;              
