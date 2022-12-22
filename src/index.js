f8299bf39fb0fb27c320a68f35da7630b57c907f
Eduarda Barboza <duuda2011@gmail.com>
2022-12-22 00:00:52 -0300

    feat: adicinoado rotas urls

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import config from './config/configuration.js';
 import accountRoute from './routes/account.route.js';
+import urlsRoute from './routes/urls.route.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(accountRoute)
+app.use(accountRoute);
+app.use(urlsRoute);
 
 const port = config.APP_PORT || 5000;
 
